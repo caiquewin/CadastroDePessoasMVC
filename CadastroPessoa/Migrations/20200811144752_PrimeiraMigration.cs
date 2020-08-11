d3b7c4ad3b5f1ec8afc43ae48604fa4ec92a39c3
Caique Pereira Alves <caiquewin@gmail.com>
2020-08-11 12:22:48 -0300

    adicionando a conexao ao sqlserver e criando o primeiro migration

---
+++ CadastroPessoa/Migrations/20200811144752_PrimeiraMigration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CadastroPessoa.Migrations
+{
+    public partial class PrimeiraMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    DataNasci = table.Column<DateTime>(nullable: false),
+                    NumeCel = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consulta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Data = table.Column<DateTime>(nullable: false),
+                    statusPagamento = table.Column<int>(nullable: false),
+                    Comentario = table.Column<string>(nullable: true),
+                    especialidades = table.Column<int>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consulta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consulta_Cliente_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Cliente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consulta_ClienteId",
+                table: "Consulta",
+                column: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consulta");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
